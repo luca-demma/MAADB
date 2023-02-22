b0c255b08b5e909a7ba801a142babb17a7726a2e
Luca Demma <demma.luca@gmail.com>
2023-02-22 20:46:04 +0100

    mongo perc WIP 2

--- mongo_perc.py
+++ mongo_perc.py
@@ -18,4 +18,16 @@ for sentiment in constants.LEXICAL_RESOURCES_FILES:
 					])
 					n_lex_words = int(n_lex_words[0]['string'])
 
+					n_twitter_words = mongo_functions.aggregate('tweet', [
+						{'$match': {'sentiment': sentiment}},
+						{'$project': {'keys': {'$objectToArray': '$word_count'}}},
+						{'$unwind': '$keys'},
+						{'$group': {'_id': '$keys.k'}},
+						{'$group': {'_id': None, 'count': {'$sum': 1}}}
+					])
+
+					print("SENTIMENT: %s	SOURCE: %s\n" % (sentiment, source))
+					# print("n_shared_words: %s" % (str(n_shared_words)))
+					print("n_lex_words: %s" % (str(n_lex_words)))
+					print("n_twitter_words: %s\n" % (str(n_twitter_words)))
 
